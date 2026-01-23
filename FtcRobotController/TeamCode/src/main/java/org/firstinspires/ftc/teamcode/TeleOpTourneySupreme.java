62e9747c67d48467fdae0c761cdf5c86ac6fde4c
Esha K <140355239+MeltySocks@users.noreply.github.com>
2026-01-22 20:43:02 -0800

    Update TeleOpTourneySupreme.java

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTourneySupreme.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTourneySupreme.java
@@ -14,7 +14,7 @@ import com.qualcomm.hardware.limelightvision.LLResultTypes;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-@TeleOp(name = "TeleOpTourneyMega")
+@TeleOp(name = "TeleOpTourneySupreme")
 public class TeleOpTourneySupreme extends LinearOpMode {
 
     // Motor power constants
